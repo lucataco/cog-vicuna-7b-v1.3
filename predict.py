f50e1a4856c0933739dbad098b8a9e130eafa22d
Luis <me@lucataco.com>
2023-06-28 01:34:07 -0400

    Modify Predict

--- predict.py
+++ predict.py
@@ -1,31 +1,34 @@
 from cog import BasePredictor, Input
 import torch
 import transformers
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+device = "cuda"
+MODEL_ID = 'lmsys/vicuna-7b-v1.3'
 
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-        name = 'lmsys/vicuna-7b-v1.3'
         self.tokenizer = AutoTokenizer.from_pretrained(
-            name,
+            MODEL_ID,
             cache_dir="cache"
         )
         self.model = AutoModelForCausalLM.from_pretrained(
-            name,
+            MODEL_ID,
+            device_map="auto",
             torch_dtype=torch.bfloat16,
             cache_dir="cache"
         )
 
     def predict(self,
         prompt: str = Input(description="Instruction for the model"),
-        max_new_tokens: int = Input(description="max tokens to generate", default=500)
+        max_new_tokens: int = Input(description="max tokens to generate", default=128),
+        temperature: float = Input(description="0.01 to 1.0 temperature", default=0.75),
     ) -> str:    
-        pipe = pipeline('text-generation', model=self.model, tokenizer=self.tokenizer, device='cuda:0')
-        with torch.autocast('cuda', dtype=torch.bfloat16):
-            output = pipe(
-                        prompt,
-                        max_new_tokens=max_new_tokens,
-                        use_cache=True
-                    )
-        return str(output)
+        inputs = self.tokenizer.encode(prompt, return_tensors="pt").to(device)
+        outputs = self.model.generate(
+            inputs, max_new_tokens=max_new_tokens, temperature=temperature
+        )
+        output = self.tokenizer.decode(outputs[0])
+        print(output)
+
+        return output
